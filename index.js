2c0844ddee8f06334526d54e59220899034e784b
Martin Eley <meley@salesforce.com>
2020-03-26 21:57:58 +0000

    Added express

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+
+const app = express();
+
+app.use(serveStatic(__dirname + "/dist"));
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
