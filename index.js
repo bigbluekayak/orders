cd82e8881489d9d70a1bbd49b75df45a36866ef3
Martin Eley <meley@salesforce.com>
2020-03-30 22:27:38 +0100

    Added app.json

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = express();
 
 app.use(serveStatic(__dirname + "/dist"));
 
+app.get('/api', (req, res) => {
+    res.sendStatus(200);
+});
+
+app.post('/api/order', (req, res) => {
+    res.sendStatus(200);
+});
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
